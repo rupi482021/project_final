019ad9cbc4a8251bdf8fd932f10d66cd01d8599a
rupi482021 <77381517+rupi482021@users.noreply.github.com>
2021-08-16 21:29:18 +0300

    LA

--- proj/proj/Global.asax.cs
+++ proj/proj/Global.asax.cs
@@ -1,23 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Web;using System.Web.Http;using System.Web.Mvc;using System.Web.Optimization;using System.Web.Routing;using System.IO;using System.Timers;using Kaatsu.Models.DAL;using System.Threading;
+
 
 namespace proj
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            static Thread keepAliveThread = new Thread(KeepAlive);
+            protected void Application_Start()
+            {
+                AreaRegistration.RegisterAllAreas();
+                GlobalConfiguration.Configure(WebApiConfig.Register);
+                FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+                RouteConfig.RegisterRoutes(RouteTable.Routes);
+                BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+                keepAliveThread.Start();
+            }
+
+            static void mail()
+            {
+
+                DBServices sentMail = new DBServices();
+                //sentMail.sentMail();
+            }
+            static void KeepAlive()
+            {
+
+                while (true)
+                {
+                    try
+                    {
+                        //mail();
+                        Thread.Sleep(1000);
+
+                    }
+                    catch (ThreadAbortException)
+                    {
+                        break;
+                    }
+                }
+            }
+
         }
-    }
 }
